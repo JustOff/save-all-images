317c719c465560365bc8dda7eceb4e3b15ada612
JustOff <Off.Just.Off@gmail.com>
2021-02-22 23:36:05 +0200

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.0.5
+set VER=1.0.6
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/save-all-images-.+?\.xpi/download\/%VER%\/save-all-images-%VER%\.xpi/" update.xml
